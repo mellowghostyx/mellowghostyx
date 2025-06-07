2413006987d3ad028dae97127aa531b43e699f79
mellowghostyx <mellowghostyx@proton.me>
2025-06-06 20:18:11 -0500

    minor tweaks to code block

--- aboutme.py
+++ aboutme.py
@@ -2,18 +2,13 @@
 # inspired by: https://github.com/Zhenye-Na/Zhenye-Na
 
 class User:
-    def __init__(self, name, pronouns, languages):
+    def __init__(self, name, pronouns, language):
         self.name = name
         self.pronouns = pronouns
-        self.languages = languages
-    
+        self.language = language
+
     def greet(self):
-        print("Hello! I'm", self.name + ", and welcome to my readme!")
+        print(f"Hello! I'm {self.name}, and welcome to my readme!")
 
-ghostyx = User(
-    name="Ghostyx",
-    pronouns="he/him",
-    languages=["en-US"]
-)
-
+ghostyx = User('Ghostyx', 'he/him', 'en-US')
 ghostyx.greet()
